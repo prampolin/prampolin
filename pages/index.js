75dfe5769df2761bec09ea17b24d2df5bcadaabe
prampolin <42499782+prampolin@users.noreply.github.com>
2021-01-23 21:37:18 -0300

    Links Fixit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Home() {
     return (
         <div>
-            <h1>Home </h1>
+            <h1>Home</h1>
             <Link href="/sobre">
                 <a>Acessar sobre</a>
             </Link>
