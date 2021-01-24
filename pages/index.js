e8187508a6a6db644409ceb5429c6b167c5a7e29
prampolin <42499782+prampolin@users.noreply.github.com>
2021-01-23 21:31:51 -0300

    Links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 function Home() {
-    return <h1>Home </h1>
+    return (
+        <div>
+            <h1>Home </h1>
+            <Link href="/sobre">
+                <a>Acessar sobre</a>
+            </Link>
+        </div>
+    )
 }
 
 export default Home
